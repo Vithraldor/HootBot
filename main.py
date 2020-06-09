7cd7da9f642c58595051e07098f1479478812b11
Vithraldor <66455041+Vithraldor@users.noreply.github.com>
2020-06-09 19:01:52 -0400

    Update main.py
    Reworked F command - now counts how many times respects were paid instead of generating a random number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 '''
-HootBot v1.1
+HootBot v1.2
 
 Developed for personal use by Vithraldor.
 This is a basic bot that mimics an owl.
@@ -7,9 +7,11 @@ This is a basic bot that mimics an owl.
 Past Versions:
 v1.0 (6/9/2020): initial release of bot.
 v1.1 (6/9/2020): minor bug fixes to F event, altered probability value for default case.
+v1.2 (6/9/2020): Reworked F event: now increments instead of randomly generating numbers
 '''
 
 import discord
+import asyncio
 import random
 from random import seed
 
@@ -19,18 +21,19 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     await client.change_presence(activity = discord.Game(name = "hoot hoot"))
+    global respectCounter
+    respectCounter = 0
 
 @client.event
 async def on_message(message):
-
     # Checks if the message was sent by an actual user so the bot won't respond to itself
     if message.author == client.user:
         return
 
     # If user types F to pay respects. A number is randomly generated between 0 and 100 to represent the 'total' amount of respects given.
-    elif message.content == 'F':
-        seed()
-        respectCounter = int(random.random() * 100)
+    elif message.content == 'F' or message.content == 'f':
+        global respectCounter
+        respectCounter += 1
         await message.channel.send('Hoot hoot!\n*Respects have been paid. Total respects paid: {}*'.format(respectCounter))
 
     # If messages contain 'who' or 'WHO'
@@ -40,7 +43,7 @@ async def on_message(message):
     elif 'WHO' in message.content:
         await message.channel.send('***HOOT HOOT***')
 
-    # Default case - probability value added to reduce annoyance.
+    # Default case. Will only send messages 33% of the time to reduce annoyance.
     else:
         seed()
         probabilityValue = int(random.random() * 100)
@@ -48,5 +51,4 @@ async def on_message(message):
         if probabilityValue >= 66:
             await message.channel.send('Hoot hoot')
 
-
-client.run('Replace this with your bot token')
+client.run('Your bot token here')
