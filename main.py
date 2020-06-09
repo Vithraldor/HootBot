574e82617c5ad5a30d62e50f2697fdc665706071
Vithraldor <66455041+Vithraldor@users.noreply.github.com>
2020-06-09 17:55:06 -0400

    added playing status

--- main.py
+++ main.py
@@ -18,6 +18,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    await client.change_presence(activity = discord.Game(name = "hoot hoot"))
 
 @client.event
 async def on_message(message):
